733149448349ff9b481bd670cafd6f4a1589d4db
PHUNG MINH HOA <hoaphung10112003@gmail.com>
2025-11-01 20:48:14 +0700

    tối ưu lại cách lưu session

--- InternalTrainingSystem.WebApp/Extensions/SessionExtensions.cs
+++ InternalTrainingSystem.WebApp/Extensions/SessionExtensions.cs
@@ -2,47 +2,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace InternalTrainingSystem.WebApp.Extensions
 {
-    public static class ControllerExtensions
-    {
-        public static bool IsUserLoggedIn(this Controller controller)
-        {
-            var userFullName = controller.HttpContext.Session.GetString("UserFullName");
-            return !string.IsNullOrEmpty(userFullName);
-        }
-
-        public static string? GetCurrentUserName(this Controller controller)
-        {
-            return controller.HttpContext.Session.GetString("UserFullName");
-        }
-
-        public static string? GetCurrentUserEmail(this Controller controller)
-        {
-            return controller.HttpContext.Session.GetString("UserEmail");
-        }
-
-        public static string? GetCurrentEmployeeId(this Controller controller)
-        {
-            return controller.HttpContext.Session.GetString("EmployeeId");
-        }
-    }
-
     public static class SessionExtensions
     {
-        public static void SetUserInfo(this ISession session, string fullName, string email, string? employeeId = null)
+        public static void SetUserInfo(this ISession session, string fullName, string email, string id)
         {
             session.SetString("UserFullName", fullName);
             session.SetString("UserEmail", email);
-            if (!string.IsNullOrEmpty(employeeId))
-            {
-                session.SetString("EmployeeId", employeeId);
-            }
+            session.SetString("Id", id);
         }
 
         public static void ClearUserInfo(this ISession session)
         {
             session.Remove("UserFullName");
             session.Remove("UserEmail");
-            session.Remove("EmployeeId");
+            session.Remove("Id");
         }
     }
 }
