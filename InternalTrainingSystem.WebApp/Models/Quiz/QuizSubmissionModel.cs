10153b1688735b709e480de003f51df092f6f2cc
PHUNG MINH HOA <hoaphung10112003@gmail.com>
2025-11-08 16:10:43 +0700

    xóa quiz

--- InternalTrainingSystem.WebApp/Models/Quiz/QuizSubmissionModel.cs
+++
@@ -1,8 +0,0 @@
-namespace InternalTrainingSystem.WebApp.Models.Quiz
-{
-    public class QuizSubmissionModel
-    {
-        public int QuizId { get; set; }
-        public Dictionary<int, int> Answers { get; set; } = new Dictionary<int, int>();
-    }
-}
